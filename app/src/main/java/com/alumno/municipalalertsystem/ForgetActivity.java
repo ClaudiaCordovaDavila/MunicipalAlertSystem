5d197cf2e501ea118b330499cdc27174c8a7f254
Alumno <rexpear@gmail.com>
2017-05-22 10:39:38 -0500

    a

--- app/src/main/java/com/alumno/municipalalertsystem/ForgetActivity.java
+++ app/src/main/java/com/alumno/municipalalertsystem/ForgetActivity.java
@@ -9,6 +9,10 @@ public class ForgetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
 
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget);
     }
