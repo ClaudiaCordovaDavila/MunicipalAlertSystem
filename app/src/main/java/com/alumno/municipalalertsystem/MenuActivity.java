55cd14e791ee8c8cbb2eb18fc0ca9f86f398eb53
Alumno <claudia.cordova@tecsup.edu.pe>
2017-05-23 09:58:49 -0500

    b

--- app/src/main/java/com/alumno/municipalalertsystem/MenuActivity.java
+++ app/src/main/java/com/alumno/municipalalertsystem/MenuActivity.java
@@ -1,16 +1,34 @@
 package com.alumno.municipalalertsystem;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Map;
+
 
 public class MenuActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        new HttpREST().execute();
+
     }
 
     public void phone(View view) {
@@ -22,4 +40,53 @@ public class MenuActivity extends AppCompatActivity {
         Intent intent = new Intent(this, sentMessageActivity.class);
         startActivity(intent);
     }
+
+
+    private class HttpREST extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            Log.i("===>", "Dentro de doInBackground()");
+            try {
+
+                final ListView lstProductos = (ListView)findViewById(R.id.listView);
+
+                HttpRequest httpRequest = HttpRequest.get("http://munical-alert.000webhostapp.com/rest_tienda/rest_tienda/index.php/news");
+
+                String respuesta = httpRequest.body().toString();
+
+                Log.i("===>", respuesta);
+
+                Gson gson = new Gson();
+                Type stringStringMap = new TypeToken<ArrayList<Map<String, Object>>>() {
+                }.getType();
+                final ArrayList<Map<String, Object>> retorno = gson.fromJson(respuesta, stringStringMap);
+
+                final String[] matriz = new String[retorno.size()];
+                int i = 0;
+
+                for (Map<String, Object> x : retorno) {
+                    matriz[i++] = (String) (x.get("newstitle") + " \n" + x.get("newsinformation"));
+                }
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                                MenuActivity.this,
+                                android.R.layout.simple_list_item_1,
+                                matriz);
+                        lstProductos.setAdapter(adaptador);
+                    }
+                });
+
+            } catch (Exception ex) {
+                Log.e("===>", "Error: " + ex);
+            }
+            return null;
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/alumno/municipalalertsystem/PhoneActivity.java
+++ app/src/main/java/com/alumno/municipalalertsystem/PhoneActivity.java
@@ -1,9 +1,19 @@
 package com.alumno.municipalalertsystem;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class PhoneActivity extends AppCompatActivity {
 
@@ -12,7 +22,7 @@ public class PhoneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone);
 
-        ListView listView1 = (ListView) findViewById(R.id.list);
+       final  ListView listView1 = (ListView) findViewById(R.id.list);
 
         String[] items = { "Fire Department                                           105", "Police Department                                      116", "Serenazgo                                               632-1300" };
 
@@ -21,6 +31,55 @@ public class PhoneActivity extends AppCompatActivity {
 
         listView1.setAdapter(adapter);
 
+        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public static final int REQUEST_CALL_PHONE = 1 ;
+
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Object o = listView1.getItemAtPosition(position);
+
+                Log.i("------->",  "" + position);
+
+
+                int checkPermission = ContextCompat.checkSelfPermission(PhoneActivity.this, Manifest.permission.CALL_PHONE);
+                if (checkPermission != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(
+                            PhoneActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE},
+                            REQUEST_CALL_PHONE);
+                }
+                else if(position==0){
+                    //position =1;
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+115"));
+                    startActivity(intent);
+                }
+                else if(position==1){
+                   // position =0;
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+116"));
+                    startActivity(intent);
+                }
+                else if(position==2){
+                    //position =2;
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+623-1300"));
+                    startActivity(intent);
+                }
+
+
+                else if (false){
+
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+911"));
+                    startActivity(intent);
+                }
+
+
+            }
+        });
+
 
     }
+
+
+
 }
