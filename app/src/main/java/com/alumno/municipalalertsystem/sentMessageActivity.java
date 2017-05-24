31b61fb9743c78ad833aa6273fa2f9dbe8c1d881
ClaudiaCordovaDavila <claudia.cordova@tecsup.edu.pe>
2017-05-24 01:38:35 -0500

    aa

--- app/src/main/java/com/alumno/municipalalertsystem/sentMessageActivity.java
+++ app/src/main/java/com/alumno/municipalalertsystem/sentMessageActivity.java
@@ -1,24 +1,34 @@
 package com.alumno.municipalalertsystem;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class sentMessageActivity extends AppCompatActivity {
+
+    private static final String TAG = sentMessageActivity.class.getSimpleName();
 
     private ImageButton btn_camera;
     private ImageView img;
@@ -30,73 +40,105 @@ public class sentMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sent_message);
 
-        //
-        img = (ImageView)this.findViewById(R.id.mostrar);
-        btn_camera = (ImageButton)this.findViewById(R.id.camera);
+        img = (ImageView)findViewById(R.id.mostrar);
 
-        btn_camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntenet = new Intent(
-                        MediaStore.ACTION_IMAGE_CAPTURE);
-                File imagesFoler = new File(
-                        Environment.getExternalStorageDirectory(),"guardar imagen");
-                imagesFoler.mkdirs();
-                File image = new File(imagesFoler,"foto,jpg");
-                Uri uriSavedImage = Uri.fromFile(image);
-                cameraIntenet.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
-                startActivityForResult(cameraIntenet,1);
+    }
+
+    private static final int CAPTURE_IMAGE_REQUEST = 300;
+
+    private Uri mediaFileUri;
+
+    public void takePicture(View view){
+        try{
+
+            if(!permissionsGranted()){
+                ActivityCompat.requestPermissions(this, PERMISSIONS_LIST, PERMISSIONS_REQUEST);
+                return;
             }
-        });
-    }
 
-    public void abrirGaleria(View v){
-        Intent intent = new Intent();
-        intent.setType("/image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(
-                Intent.createChooser(intent, "Seleccione una imagen"),
-                SELECT_FILE);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent){
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-        Uri selectedImageUri = null;
-        Uri selectedImage;
-
-        String filePath = null;
-        switch (requestCode) {
-            case SELECT_FILE:
-                if (resultCode == Activity.RESULT_OK) {
-                    selectedImage = imageReturnedIntent.getData();
-                    String selectedPath=selectedImage.getPath();
-                    if (requestCode == SELECT_FILE) {
-
-                        if (selectedPath != null) {
-                            InputStream imageStream = null;
-                            try {
-                                imageStream = getContentResolver().openInputStream(
-                                        selectedImage);
-                            } catch (FileNotFoundException e) {
-                                e.printStackTrace();
-                            }
-
-                            // Transformamos la URI de la imagen a inputStream y este a un Bitmap
-                            Bitmap bmp = BitmapFactory.decodeStream(imageStream);
-
-                            // Ponemos nuestro bitmap en un ImageView que tengamos en la vista
-                            ImageView mImg = (ImageView) findViewById(R.id.memory);
-                            mImg.setImageBitmap(bmp);
-
-                        }
-                    }
+            // Creando el directorio de imágenes (si no existe)
+            File mediaStorageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            if(!mediaStorageDir.exists()){
+                if(!mediaStorageDir.mkdirs()) {
+                    throw new Exception("Failed to create directory");
                 }
-                break;
+            }
+
+            // Definiendo la ruta destino de la captura (Uri)
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+            mediaFileUri = Uri.fromFile(mediaFile);
+
+            // Iniciando la captura
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, mediaFileUri);
+            startActivityForResult(intent, CAPTURE_IMAGE_REQUEST);
+
+        }catch (Exception e){
+            Log.e(TAG, e.toString());
+            Toast.makeText(this, "Error en captura: " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
-    public void submitmessage(View view) {
-        Intent intent = new Intent(this, SuccessSentActivity.class);
-        startActivity(intent);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == CAPTURE_IMAGE_REQUEST){
+            // Resultado en la captura de la foto
+            if(resultCode == RESULT_OK){
+                try {
+                    Log.d(TAG, "ResultCode: RESULT_OK");
+                    // Toast.makeText(this, "Image saved to: " + mediaFileUri.getPath(), Toast.LENGTH_LONG).show();
+
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), mediaFileUri);
+                    img.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    Log.d(TAG, e.toString());
+                    Toast.makeText(this, "Error al procesar imagen: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }else if(resultCode == RESULT_CANCELED){
+                Log.d(TAG, "ResultCode: RESULT_CANCELED");
+            }else{
+                Log.d(TAG, "ResultCode: " + resultCode);
+            }
+        }
     }
+
+    /**
+     * Permissions handler
+     */
+
+    private static final int PERMISSIONS_REQUEST = 200;
+
+    private static String[] PERMISSIONS_LIST = new String[]{
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    private boolean permissionsGranted(){
+        for (String permission : PERMISSIONS_LIST){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+        switch (requestCode){
+            case PERMISSIONS_REQUEST: {
+                for (int i=0; i<grantResults.length; i++){
+                    Log.d(TAG, ""+grantResults[i]);
+                    if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this, PERMISSIONS_LIST[i] + " permiso rechazado!", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+                Toast.makeText(this, "Permisos concedidos, intente nuevamente.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
 }
